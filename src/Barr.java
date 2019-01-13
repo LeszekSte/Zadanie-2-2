f0479701ff5f9317d80a00731df030233463ca58
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-01-13 16:06:04 +0100

    Wersja z Lenovo

--- src/Barr.java
+++ src/Barr.java
@@ -5,20 +5,38 @@ public class Barr {
         drink1.price = 8.50;
         drink1.alcohol = true;
 
+        drink1.composition1 = new Composition();
+        drink1.composition1.compsition = "Wodka";
+        drink1.composition1.qunatity = 50;
+
+        drink1.composition2 = new Composition();
+        drink1.composition2.compsition = "Coca-Cola";
+        drink1.composition2.qunatity = 60;
+
+        drink1.composition3 = new Composition();
+        drink1.composition3.compsition = "Martni";
+        drink1.composition3.qunatity = 70;
+
+
+        double capacityDrink = drink1.composition2.qunatity + drink1.composition2.qunatity + drink1.composition3.qunatity;
 
         int nrDrinka = 0;
 
         System.out.println("Drink nr  - " + ++nrDrinka);
-        System.out.printf("Nazwa            %s\n", drink1.name);
-        System.out.printf("Cena             %.2f zł\n", drink1.price);
+        System.out.printf("Nazwa:\t\t\t\t\t%s\n", drink1.name);
 
         String alkoholPoPolsku = "nie";
-
         if (drink1.alcohol) {
             alkoholPoPolsku = "Tak";
         }
+        System.out.printf("\nZawiera alkohol  \t\t\t- %s\n\n", alkoholPoPolsku);
+        System.out.printf("Cena\t\t\t\t\t\t- %.2f zł\n\n", drink1.price);
+        System.out.printf("Sladnik 1\t\t%s\t\t- %.0f ml\n", drink1.composition1.compsition, drink1.composition1.qunatity);
+        System.out.printf("Sladnik 2\t\t%s\t- %.0f ml\n", drink1.composition2.compsition, drink1.composition2.qunatity);
+        System.out.printf("Sladnik 3\t\t%s\t\t- %.0f ml\n", drink1.composition3.compsition, drink1.composition3.qunatity);
+        System.out.printf("Pojemność Drinka \t\t\t- %.0f ml\n", capacityDrink);
 
-        System.out.printf("Zawiera alkohol  %s\n", alkoholPoPolsku);
+
     }
 
 }
